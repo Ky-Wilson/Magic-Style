0702715b7bc6a2e3e5bfa88846a68c868cb49d3c
Ky-Wilson <yanne.kouassi@epitech.eu>
2025-08-19 17:17:15 +0000

    Commit Node.js | Modifications: .vite,

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -1,9 +1,11 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Surfsidemedia\Shoppingcart\Facades\Cart;
 
 class ShopController extends Controller
@@ -11,7 +13,12 @@ class ShopController extends Controller
     public function index(Request $request)
     {
         $size = $request->query('size', 12);
+        $categories = Category::orderBy('name', 'ASC')->get();
         $order = $request->query('order', -1);
+        $f_brands = $request->query('brands', '');
+        $f_categories = $request->query('categories', '');
+        $min_price = $request->query('min_price') ? $request->query('min_price') : 1;
+        $max_price = $request->query('max_price') ? $request->query('max_price') : 150000;
 
         $o_column = 'id';
         $o_order = 'DESC';
@@ -25,18 +32,42 @@ class ShopController extends Controller
                 $o_order = 'ASC';
                 break;
             case 3:
-                $o_column = 'regular_price';
+                $o_column = 'sale_price';
                 $o_order = 'ASC';
                 break;
             case 4:
-                $o_column = 'regular_price';
+                $o_column = 'sale_price';
                 $o_order = 'DESC';
                 break;
         }
 
-        $products = Product::orderBy($o_column, $o_order)->paginate($size);
+        $brands = Brand::orderBy('name', 'ASC')->get();
+        $productsQuery = Product::query();
 
-        return view('shop', compact('products', 'size', 'order'));
+        // Appliquer le filtre des marques
+        if (!empty($f_brands)) {
+            $brandIds = explode(',', $f_brands);
+            $productsQuery->whereIn('brand_id', $brandIds);
+        }
+        
+        // Appliquer le filtre de prix
+        $productsQuery->where(function ($query) use ($min_price, $max_price) {
+            $query->whereBetween('regular_price', [$min_price, $max_price])
+                  ->orWhere(function ($q) use ($min_price, $max_price) {
+                      $q->whereNotNull('sale_price')
+                        ->whereBetween('sale_price', [$min_price, $max_price]);
+                  });
+        });
+
+        // Appliquer le filtre des catégories
+        if (!empty($f_categories)) {
+            $categoryIds = explode(',', $f_categories);
+            $productsQuery->whereIn('category_id', $categoryIds);
+        }
+
+        $products = $productsQuery->orderBy($o_column, $o_order)->paginate($size);
+
+        return view('shop', compact('products', 'size', 'order', 'brands', 'f_brands', 'categories', 'f_categories', 'min_price', 'max_price'));
     }
 
     public function product_details($product_slug)
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,10 +1,6 @@
-
-
-
-
-
 @extends('layouts.app')
 @section('content')
+
      <main class="pt-90">
     <section class="shop-main container d-flex pt-4 pt-xl-5">
       <div class="shop-sidebar side-sticky bg-body" id="shopFilter">
@@ -31,25 +27,25 @@
             </h5>
             <div id="accordion-filter-1" class="accordion-collapse collapse show border-0"
               aria-labelledby="accordion-heading-1" data-bs-parent="#categories-list">
-              <div class="accordion-body px-0 pb-0 pt-3">
+              <div class="accordion-body px-0 pb-0 pt-3 category-list">
                 <ul class="list list-inline mb-0">
-                  <li class="list-item"><a href="#" class="menu-link py-1">Dresses</a></li>
-                  <li class="list-item"><a href="#" class="menu-link py-1">Shorts</a></li>
-                  <li class="list-item"><a href="#" class="menu-link py-1">Sweatshirts</a></li>
-                  <li class="list-item"><a href="#" class="menu-link py-1">Swimwear</a></li>
-                  <li class="list-item"><a href="#" class="menu-link py-1">Jackets</a></li>
-                  <li class="list-item"><a href="#" class="menu-link py-1">T-Shirts & Tops</a></li>
-                  <li class="list-item"><a href="#" class="menu-link py-1">Jeans</a></li>
-                  <li class="list-item"><a href="#" class="menu-link py-1">Trousers</a></li>
-                  <li class="list-item"><a href="#" class="menu-link py-1">Men</a></li>
-                  <li class="list-item"><a href="#" class="menu-link py-1">Jumpers & Cardigans</a></li>
+                  @foreach($categories as $category)
+                      <li class="list-item">
+                        <span class="menu-link py-1">
+                          <input type="checkbox" class="chk-category" name="categories" value="{{$category->id}}"
+                          @if(in_array($category->id, explode(',', $f_categories))) checked="checked" @endif
+                          >
+                          {{$category->name}}
+                        </span>
+                        <span class="text-right float-end">{{ $category->products->count() }}</span>
+                  @endforeach
                 </ul>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="accordion" id="color-filters">
+       {{--  <div class="accordion" id="color-filters">
           <div class="accordion-item mb-4 pb-3">
             <h5 class="accordion-header" id="accordion-heading-1">
               <button class="accordion-button p-0 border-0 fs-5 text-uppercase" type="button" data-bs-toggle="collapse"
@@ -111,7 +107,7 @@
               </div>
             </div>
           </div>
-        </div>
+        </div> --}}
 
         <div class="accordion" id="brand-filters">
           <div class="accordion-item mb-4 pb-3">
@@ -127,89 +123,58 @@
                 </svg>
               </button>
             </h5>
-            <div id="accordion-filter-brand" class="accordion-collapse collapse show border-0"
-              aria-labelledby="accordion-heading-brand" data-bs-parent="#brand-filters">
+            <div id="accordion-filter-brand" class="accordion-collapse collapse show border-0" aria-labelledby="accordion-heading-brand" data-bs-parent="#brand-filters">
               <div class="search-field multi-select accordion-body px-0 pb-0">
-                <select class="d-none" multiple name="total-numbers-list">
-                  <option value="1">Adidas</option>
-                  <option value="2">Balmain</option>
-                  <option value="3">Balenciaga</option>
-                  <option value="4">Burberry</option>
-                  <option value="5">Kenzo</option>
-                  <option value="5">Givenchy</option>
-                  <option value="5">Zara</option>
-                </select>
-                <div class="search-field__input-wrapper mb-3">
-                  <input type="text" name="search_text"
-                    class="search-field__input form-control form-control-sm border-light border-2"
-                    placeholder="Search" />
-                </div>
-                <ul class="multi-select__list list-unstyled">
-                  <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                    <span class="me-auto">Adidas</span>
-                    <span class="text-secondary">2</span>
-                  </li>
-                  <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                    <span class="me-auto">Balmain</span>
-                    <span class="text-secondary">7</span>
-                  </li>
-                  <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                    <span class="me-auto">Balenciaga</span>
-                    <span class="text-secondary">10</span>
-                  </li>
-                  <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                    <span class="me-auto">Burberry</span>
-                    <span class="text-secondary">39</span>
-                  </li>
-                  <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                    <span class="me-auto">Kenzo</span>
-                    <span class="text-secondary">95</span>
-                  </li>
-                  <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                    <span class="me-auto">Givenchy</span>
-                    <span class="text-secondary">1092</span>
-                  </li>
-                  <li class="search-suggestion__item multi-select__item text-primary js-search-select js-multi-select">
-                    <span class="me-auto">Zara</span>
-                    <span class="text-secondary">48</span>
-                  </li>
-                </ul>
+               <ul class="list list-inline mb-0 brand-list">
+                    @foreach($brands as $brand)
+                      <li class="list-item">
+                        <span class="menu-link py-1">
+                          <input type="checkbox" name="brand" value="{{$brand->id}}" id="brand-{{$brand->id}}" class="chk-brand"  @if(in_array($brand->id, explode(',', $f_brands))) checked= "checked" @endif>
+                         
+                          {{$brand->name}}
+                        </span>
+                        <span class="text-right float-end">
+                          {{ $brand->products->count() }}
+                        </span>
+                      </li>
+                    @endforeach
+               </ul>
               </div>
             </div>
           </div>
         </div>
 
         <div class="accordion" id="price-filters">
-          <div class="accordion-item mb-4">
-            <h5 class="accordion-header mb-2" id="accordion-heading-price">
-              <button class="accordion-button p-0 border-0 fs-5 text-uppercase" type="button" data-bs-toggle="collapse"
+    <div class="accordion-item mb-4">
+        <h5 class="accordion-header mb-2" id="accordion-heading-price">
+            <button class="accordion-button p-0 border-0 fs-5 text-uppercase" type="button" data-bs-toggle="collapse"
                 data-bs-target="#accordion-filter-price" aria-expanded="true" aria-controls="accordion-filter-price">
                 Price
                 <svg class="accordion-button__icon type2" viewBox="0 0 10 6" xmlns="http://www.w3.org/2000/svg">
-                  <g aria-hidden="true" stroke="none" fill-rule="evenodd">
-                    <path
-                      d="M5.35668 0.159286C5.16235 -0.053094 4.83769 -0.0530941 4.64287 0.159286L0.147611 5.05963C-0.0492049 5.27473 -0.049205 5.62357 0.147611 5.83813C0.344427 6.05323 0.664108 6.05323 0.860924 5.83813L5 1.32706L9.13858 5.83867C9.33589 6.05378 9.65507 6.05378 9.85239 5.83867C10.0492 5.62357 10.0492 5.27473 9.85239 5.06018L5.35668 0.159286Z" />
-                  </g>
+                    <g aria-hidden="true" stroke="none" fill-rule="evenodd">
+                        <path
+                            d="M5.35668 0.159286C5.16235 -0.053094 4.83769 -0.0530941 4.64287 0.159286L0.147611 5.05963C-0.0492049 5.27473 -0.049205 5.62357 0.147611 5.83813C0.344427 6.05323 0.664108 6.05323 0.860924 5.83813L5 1.32706L9.13858 5.83867C9.33589 6.05378 9.65507 6.05378 9.85239 5.83867C10.0492 5.62357 10.0492 5.27473 9.85239 5.06018L5.35668 0.159286Z" />
+                    </g>
                 </svg>
-              </button>
-            </h5>
-            <div id="accordion-filter-price" class="accordion-collapse collapse show border-0"
-              aria-labelledby="accordion-heading-price" data-bs-parent="#price-filters">
-              <input class="price-range-slider" type="text" name="price_range" value="" data-slider-min="10"
-                data-slider-max="1000" data-slider-step="5" data-slider-value="[250,450]" data-currency="$" />
-              <div class="price-range__info d-flex align-items-center mt-2">
+            </button>
+        </h5>
+        <div id="accordion-filter-price" class="accordion-collapse collapse show border-0"
+            aria-labelledby="accordion-heading-price" data-bs-parent="#price-filters">
+            <input class="price-range-slider" type="text" name="price_range" value="" data-slider-min="1"
+                data-slider-max="150000" data-slider-step="100" data-slider-value="[{{ $min_price }},{{ $max_price }}]" data-currency="$" />
+            <div class="price-range__info d-flex align-items-center mt-2">
                 <div class="me-auto">
-                  <span class="text-secondary">Min Price: </span>
-                  <span class="price-range__min">$250</span>
+                    <span class="text-secondary">Min Price: </span>
+                    <span class="price-range__min">${{ $min_price }}</span>
                 </div>
                 <div>
-                  <span class="text-secondary">Max Price: </span>
-                  <span class="price-range__max">$450</span>
+                    <span class="text-secondary">Max Price: </span>
+                    <span class="price-range__max">${{ $max_price }}</span>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
+    </div>
+</div>
       </div>
 
       <div class="shop-list flex-grow-1">
@@ -440,34 +405,72 @@
     <input type="hidden" name="page" id="page" value="{{ $products->currentPage() }}">
     <input type="hidden" name="size" id="size" value="{{ $size }}">
     <input type="hidden" name="order" id="order" value="{{ $order }}">
-  </form>
+    <input type="hidden" name="brands" id="hdnbrands" value="{{ $f_brands }}">
+    <input type="hidden" name="categories" id="hdncategories" value="{{ $f_categories }}">
+    <input type="hidden" name="min_price" id="hdnMinPrice" value="{{ $min_price }}">
+    <input type="hidden" name="max_price" id="hdnMaxPrice" value="{{ $max_price }}">
+</form>
 @endsection
 @push('scripts')
-  <script>
-    $(document).ready(function() {
-      // Handle page size change
-      $('#pagesize').on('change', function() {
-        $('#size').val($(this).val());
-        $('#page').val(1); // Reset to first page when changing size
-        $('#frmfilter').submit();
-      });
+    <script>
+        $(document).ready(function() {
+            // Handle page size change
+            $('#pagesize').on('change', function() {
+                $('#size').val($(this).val());
+                $('#page').val(1); // Reset to first page when changing size
+                $('#frmfilter').submit();
+            });
 
-      // Handle order change
-      $('#orderby').on('change', function() {
-        $('#order').val($(this).val());
-        $('#page').val(1); // Reset to first page when changing order
-        $('#frmfilter').submit();
-      });
+            // Handle order change
+            $('#orderby').on('change', function() {
+                $('#order').val($(this).val());
+                $('#page').val(1); // Reset to first page when changing order
+                $('#frmfilter').submit();
+            });
 
-      // Handle pagination clicks
-      $('#pagination').on('click', '.page-link', function(e) {
-        e.preventDefault();
-        const page = $(this).attr('href')?.match(/page=(\d+)/)?.[1] || 1;
-        $('#page').val(page);
-        $('#frmfilter').submit();
-      });
-    });
-  </script>
+            // Handle brands
+            $("input[name='brand']").on("change", function() {
+                var brands = [];
+                $("input[name='brand']:checked").each(function() {
+                    brands.push($(this).val());
+                });
+                $('#hdnbrands').val(brands.join(','));
+                $('#page').val(1); // Reset to first page when changing brands
+                $('#frmfilter').submit();
+            });
+
+            // Handle categories
+            $("input[name='categories']").on("change", function() {
+                var categories = [];
+                $("input[name='categories']:checked").each(function() {
+                    categories.push($(this).val());
+                });
+                $('#hdncategories').val(categories.join(','));
+                $('#page').val(1); // Reset to first page when changing categories
+                $('#frmfilter').submit();
+            });
+
+
+            // Handle price range slider
+            $('.price-range-slider').on('change', function() {
+                var values = $(this).val().split(',');
+                $('#hdnMinPrice').val(values[0]);
+                $('#hdnMaxPrice').val(values[1]);
+                $('#page').val(1); // Reset to first page when changing price range
+               setTimeout(function() {
+                    $('#frmfilter').submit();
+                }, 1000); // Delay to allow slider to finish moving
+            });
+
+            // Handle pagination clicks
+            $('#pagination').on('click', '.page-link', function(e) {
+                e.preventDefault();
+                const page = $(this).attr('href')?.match(/page=(\d+)/)?.[1] || 1;
+                $('#page').val(page);
+                $('#frmfilter').submit();
+            });
+        });
+    </script>
 @endpush
 
 
